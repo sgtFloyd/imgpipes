b6c83c2d5f804fdc9e88271dff561c20dbd85171
Gabe Smith <gabe@paperlesspost.com>
2013-01-31 13:53:27 -0500

    underscore for convenience

--- web.js
+++ web.js
@@ -1,11 +1,28 @@
 var express = require('express'),
-    imagick = require('imagemagick');
+    imagick = require('imagemagick'),
+    _ = require('underscore');
 
-var app = express.createServer(express.logger());
+var app = express.createServer(express.logger()),
+    ip = {
+      VALID_ACTIONS: ['flip'],
+      isValidAction: function(action){
+        return _.contains(ip.VALID_ACTIONS, action);
+      },
+      isValidURL: function(url){
+      },
+      parseActions: function(actions){
+        if(actions) {
+          return _.filter(actions.split(','), ip.isValidAction);
+        } else return [];
+      }
+    };
 
 app.get('/', function(req, res) {
-  var url = req.query['url'];
-  res.send('URL: ' + url);
+  var url = req.query['url'],
+      actions = ip.parseActions(req.query['do']);
+
+  if( url && _.isEmpty(actions) ) return res.redirect(url);
+  res.send("URL: "+url+", Actions: "+actions);
 });
 
 var port = process.env.PORT || 5000;
