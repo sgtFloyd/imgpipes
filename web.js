897e1af381756de6f21677b1f2d7e1d8d74df4fc
Gabe <sgt.floydpepper@gmail.com>
2013-01-31 23:26:56 -0500

    steams, buffers, and pipes

--- web.js
+++ web.js
@@ -9,8 +9,6 @@ var app = express.createServer(express.logger()),
       isValidAction: function(action){
         return _.contains(ip.VALID_ACTIONS, action);
       },
-      isValidURL: function(url){
-      },
       parseActions: function(actions){
         if(actions) {
           return _.filter(actions.split(','), ip.isValidAction);
@@ -22,9 +20,33 @@ app.get('/', function(req, res) {
   var url = req.query['url'],
       actions = ip.parseActions(req.query['do']);
 
-  if( url && _.isEmpty(actions) ) return res.redirect(url);
-  res.send("URL: "+url+", Actions: "+actions);
+  if( url && _.isEmpty(actions) )
+    return request.get(url).pipe(res);
+    // return res.redirect(url);
+
+  var input = request.get(url),
+      output = new Buffer(0, 'binary'),
+      type, isImage;
+
+  input.on('data', function(buf){
+    if( typeof type == "undefined" ) {
+      type = input.response.headers['content-type'];
+      isImage = !!type.match(/^image/);
+    }
+
+    if( isImage )
+      output = Buffer.concat([output, buf]);
+  });
+
+  input.on('end', function(){
+    if( isImage ) {
+      res.contentType(type);
+      res.send(output);
+    }
+  });
+
 });
+
 app.get('/ping', function(req, res) { res.send("PONG"); });
 
 var port = process.env.PORT || 5000;
