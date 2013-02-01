4349ba8f2d7d353e87b5e88e519bdf321d88d8ef
Gabe Smith <gabe@paperlesspost.com>
2013-02-01 18:54:46 -0500

    more effects

--- web.js
+++ web.js
@@ -7,14 +7,21 @@ var app = express.createServer(express.logger());
 var EFFECTS = {
       blur:     ['convert', ['-blur', '0x3']],
       contrast: ['convert', ['-contrast']],
+      explode:  ['convert', ['-implode', '-0.5']],
+      implode:  ['convert', ['-implode', '0.33']],
       flip:     ['convert', ['-flip']],
       flop:     ['convert', ['-flop']],
+      gray:     ['convert', ['-colorspace', 'Gray']],
       negate:   ['convert', ['-negate']],
       paint:    ['convert', ['-paint', '3']],
+      polaroid: ['convert', ['-polaroid', '3', '-background', 'None', '-format', 'png']],
+      posterize: ['convert', ['-posterize', '5']],
       sepia:    ['convert', ['-sepia-tone', '75%']],
       sharpen:  ['convert', ['-sharpen', '5']],
-      swirl:    ['convert', ['-swirl', '90']]
+      swirl:    ['convert', ['-swirl', '90']],
+      vignette: ['convert', ['-vignette', '0x50']]
     };
+EFFECTS.grey = EFFECTS.greyscale = EFFECTS.grayscale = EFFECTS.gray
 EFFECTS.mirror = EFFECTS.flop;
 EFFECTS.negative = EFFECTS.invert = EFFECTS.negate;
 
